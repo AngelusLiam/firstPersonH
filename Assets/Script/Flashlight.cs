c3c2cab43cabc72986f426dd890030e853246924
Giuseppe Loffredo <giuseppe0893_1993@libero.it>
2020-06-13 14:44:24 +0200

    -m

---
+++ Assets/Script/Flashlight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public Light flashLight;
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            if (flashLight.enabled)
+            {
+                flashLight.enabled = false;
+            }
+            else
+            {
+                flashLight.enabled = true;
+            }
+        }
+    }
+}
